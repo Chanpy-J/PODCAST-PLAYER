b1b4b432b69ae44c6118eb9c2f446ebbf9080270
eugene08976 <eugenelandct@gmail.com>
2022-12-11 14:03:19 -0500

    Load wav file to list box

--- proj.py
+++ proj.py
@@ -1,17 +1,12 @@
-# Tk_demo_02_buttons.py
-# TKinter demo
-# Play a sinusoid using Pyaudio. Use buttons to adjust the frequency.
-
 from math import cos, pi 
 import pyaudio, struct
-import tkinter as Tk   	
+import tkinter as Tk    
 import time
 import wave
+import os
 
-# Open wave file (mono)
-input_wavefile = 'test.wav'
-# input_wavefile = 'sin01_mono.wav'
-# input_wavefile = 'sin01_stereo.wav'
+# Open wave file 
+input_wavefile = './podcast/test2.wav'
 
 wf          = wave.open( input_wavefile, 'rb')
 RATE        = wf.getframerate()
@@ -19,6 +14,16 @@ WIDTH       = wf.getsampwidth()
 LEN         = wf.getnframes() 
 CHANNELS    = wf.getnchannels() 
 NFRAME      = wf.getnframes()
+
+# Create Pyaudio object
+p = pyaudio.PyAudio()
+stream = p.open(
+    format = pyaudio.paInt16,  
+    channels = CHANNELS, 
+    rate = RATE,
+    input = False, 
+    output = True,
+    frames_per_buffer = 128) 
 
 def fun_pp():
   global play
@@ -38,13 +43,12 @@ def fun_quit():
 # Define TK root
 root = Tk.Tk()
 root.geometry("300x250")
-#content = Tk.Frame(root)
-#frame = Tk.Frame(content, borderwidth=5, relief="ridge", width=200, height=100)
+
 # Define widgets
 t = Tk.DoubleVar()
 t.set(0)
 list_box = Tk.Listbox(root, bg = 'black', fg = 'green', width = 20)
-slid = Tk.Scale(root,from_= 0, to = (NFRAME/RATE), orient = Tk.HORIZONTAL, variable = t )
+slid = Tk.Scale(root,from_= 0.0, to = (NFRAME / RATE), orient = Tk.HORIZONTAL, variable = t )
 B_pp = Tk.Button(root, text = 'play/pause', command = fun_pp)
 B_quit = Tk.Button(root, text = 'Quit', command = fun_quit)
 
@@ -53,17 +57,7 @@ list_box.pack(side = Tk.TOP)
 slid.pack(side = Tk.TOP, fill =Tk.BOTH)
 B_pp.pack(side = Tk.LEFT)
 B_quit.pack(side = Tk.LEFT)
-
-# Create Pyaudio object
-p = pyaudio.PyAudio()
-stream = p.open(
-    format = pyaudio.paInt16,  
-    channels = CHANNELS, 
-    rate = RATE,
-    input = False, 
-    output = True,
-    frames_per_buffer = 128)            
-    # specify low frames_per_buffer to reduce latency
+           
 
 BLOCKLEN =int(RATE/10)
 output_block = [0] * BLOCKLEN  # create 1D array  
@@ -71,27 +65,83 @@ CONTINUE = True
 play = True
 pre = 0
 
+os.chdir("./podcast")
+podcasts = os.listdir()
+for p in podcasts:
+  if p != ".DS_Store":
+    list_box.insert('end', p)
+
+list_box.selection_clear(0, 'end')
+
+prev_p = "test2.wav"
+
 while CONTINUE:
   root.update()
   root.title('podcast player')
+  
+  currentPodcast = list_box.get('active')
+  podcast_path = os.getcwd() + "/" + currentPodcast
+  print(currentPodcast)
+
+  
   if play:
-    binary_data = wf.readframes(BLOCKLEN)
-    input_block = struct.unpack('h' * BLOCKLEN, binary_data)
-    if(pre+0.1 != t.get()):
-      wf.setpos(int(t.get()*RATE))
-    pre = t.get() 
-    t.set(t.get() + 0.1)
+    if currentPodcast != prev_p:
+      t.set(0)
+      wf          = wave.open(podcast_path, 'rb')
+      RATE        = wf.getframerate()
+      WIDTH       = wf.getsampwidth()
+      LEN         = wf.getnframes() 
+      CHANNELS    = wf.getnchannels() 
+      NFRAME      = wf.getnframes()
+
+      slid.configure(to = NFRAME / RATE)
+
+      p = pyaudio.PyAudio()
+      stream = p.open(
+          format = pyaudio.paInt16,  
+          channels = CHANNELS, 
+          rate = RATE,
+          input = False, 
+          output = True,
+          frames_per_buffer = 128)
+      prev_p = currentPodcast
+
+    
+    if CHANNELS == 1:
+      binary_data = wf.readframes(BLOCKLEN)
+      input_block = struct.unpack('h' * BLOCKLEN, binary_data)
+      if(pre + 0.1 != t.get()):
+        wf.setpos(int(t.get() * RATE))
+      pre = t.get() 
+      t.set(t.get() + 0.1)
+
+      if pre >= (NFRAME / RATE):
+        play = False
+        CONTINUE = False
 
 
+      output_block = input_block
 
-    output_block = input_block
+      binary_data = struct.pack('h' * BLOCKLEN, *output_block)   # 'h' for 16 bits
+      stream.write(binary_data)
+    else:
+      binary_data = wf.readframes(BLOCKLEN)
+      input_block = struct.unpack('hh' * BLOCKLEN, binary_data)
+      if(pre + 0.1 != t.get()):
+        wf.setpos(int(t.get() * RATE))
+      pre = t.get() 
+      t.set(t.get() + 0.1)
+
+      if pre >= (NFRAME / RATE):
+        play = False
+        CONTINUE = False
 
 
+      output_block = input_block
 
-    binary_data = struct.pack('h' * BLOCKLEN, *output_block)
+      binary_data = struct.pack('hh' * BLOCKLEN, *output_block)
+      stream.write(binary_data)
 
-    binary_data = struct.pack('h' * BLOCKLEN, *output_block)   # 'h' for 16 bits
-    stream.write(binary_data)
 
 print('* Finished')
 
