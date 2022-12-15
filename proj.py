cb3032ca096022e39949ecfa60ceb3852854d05c
Chanpy-J <cc7283@nyu.edu>
2022-12-15 11:15:47 -0500

    look for bug and other thing to do

--- proj.py
+++ proj.py
@@ -7,7 +7,7 @@ import os
 import numpy as np
 
 # Open wave file 
-input_wavefile = './podcast/test2.wav'
+input_wavefile = './podcast/test.wav'
 
 wf          = wave.open(input_wavefile, 'rb')
 RATE        = wf.getframerate()
@@ -30,6 +30,7 @@ def replay():
   global podcast_path
   t.set(0)
   wf.setpos(0)
+  play = True
   #wf          = wave.open(podcast_path, 'rb')
   #RATE        = wf.getframerate()
   #WIDTH       = wf.getsampwidth()
@@ -48,7 +49,6 @@ def replay():
       #input = False, 
       #output = True,
       #frames_per_buffer = 128)
-  play = True
 
 
 def fun_pp():
@@ -66,31 +66,58 @@ def fun_quit():
   print('Good bye')
   CONTINUE = False
 
+def fun_sf():
+  global sf
+  if sf:
+    sf = False
+    print("smart fasten is off")
+    pass
+  else :
+    sf = True
+    print("smart fasten is on")
+  pass
+
 # Define TK root
 root = Tk.Tk()
-root.geometry("300x250")
-
+root.geometry("300x310")
+gain = Tk.IntVar()
+gain.set(10)
 # Define widgets
 t = Tk.DoubleVar()
 t.set(0)
+sv = Tk.DoubleVar()
+sv.set(0)
+
 list_box = Tk.Listbox(root, bg = 'black', fg = 'green', width = 20)
 slid = Tk.Scale(root,from_= 0.0, to = (NFRAME / RATE), orient = Tk.HORIZONTAL, variable = t )
 B_pp = Tk.Button(root, text = 'play/pause', command = fun_pp)
 B_rp = Tk.Button(root, text = 'replay', command = replay)
 B_quit = Tk.Button(root, text = 'Quit', command = fun_quit)
+B_fasten = Tk.Button(root, text = "smart fasten", command = fun_sf)
+S_gain = Tk.Scale(root, label = 'volumn', variable = gain, from_ = 0, to = 100, orient = Tk. HORIZONTAL)
+L_timesave = Tk.Label(root , textvariable =sv)
+L_tsl = Tk.Label(root, text = "save time")
+
+
 
 # Place widgets                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 list_box.pack(side = Tk.TOP)
+L_tsl.pack(side= Tk.TOP)
+L_timesave.pack(side = Tk.TOP)
 slid.pack(side = Tk.TOP, fill =Tk.BOTH)
 B_pp.pack(side = Tk.LEFT)
 B_rp.pack(side = Tk.LEFT)
 B_quit.pack(side = Tk.LEFT)
+B_fasten.pack(side = Tk.LEFT)
+
+S_gain.pack(side = Tk.TOP)
            
 
 BLOCKLEN =int(RATE/10)
 output_block = [0] * BLOCKLEN  # create 1D array  
 CONTINUE = True
 play = True
+sf = False
 pre = 0
 
 os.chdir("./podcast")
@@ -114,6 +141,7 @@ while CONTINUE:
   #print(currentPodcast)
   if currentPodcast != prev_p:
     t.set(0)
+    sv.set(0)
     wf          = wave.open(podcast_path, 'rb')
     RATE        = wf.getframerate()
     WIDTH       = wf.getsampwidth()
@@ -121,6 +149,7 @@ while CONTINUE:
     CHANNELS    = wf.getnchannels() 
     NFRAME      = wf.getnframes()
     BLOCKLEN = int(RATE / 10)
+    output_block = [0] * BLOCKLEN  # create 1D array  
 
     slid.configure(to = NFRAME / RATE)
 
@@ -146,18 +175,23 @@ while CONTINUE:
         print("pause")
         continue
 
+
+
       
       skip = True
       input_block = struct.unpack('h' * BLOCKLEN, binary_data)
       X = np.fft.fft(input_block)
       freqbeg = int(300/RATE*BLOCKLEN)
       freqend = int(1500/RATE*BLOCKLEN)
-      for x in range(freqbeg, freqend):
-        if int(abs(X[x])) > 10000:
-          #print(X[x])
-          skip = False
-          break
-        pass
+      if not sf :
+        skip = False 
+      else :
+        for x in range(freqbeg, freqend):
+          if int(abs(X[x])) > 10000 :
+            #print(X[x])
+            skip = False
+            break
+          pass
 
 
       if(pre + 0.1 != t.get()):
@@ -169,13 +203,15 @@ while CONTINUE:
         play = False
         #CONTINUE = False
 
-
-      output_block = input_block
-
+      for i in range(0, len(input_block)):
+        output_block[i] =int( input_block[i] * (gain.get() /10))
+      #output_block = input_block
       binary_data = struct.pack('h' * BLOCKLEN, *output_block)   # 'h' for 16 bits
-      #print(skip)
-      if not skip :
+      if not skip:
         stream.write(binary_data)
+      else:
+        sv.set(sv.get() + 0.1)
+        print(sv.get())
     else:
       binary_data = wf.readframes(BLOCKLEN)
       if len(binary_data) != BLOCKLEN * 2 * CHANNELS:
@@ -184,9 +220,19 @@ while CONTINUE:
         continue
 
 
-      skip = False
+      skip = True
       input_block = struct.unpack('hh' * BLOCKLEN, binary_data)
       X = np.fft.fft(input_block)
+      freqbeg = int(300/RATE*BLOCKLEN*CHANNELS)
+      freqend = int(1500/RATE*BLOCKLEN*CHANNELS)
+      for x in range(freqbeg, freqend):
+        if not sf:
+          skip = False
+        if int(abs(X[x])) > 40000:
+          #print(abs(X[x]))
+          skip = False
+          break
+        pass
       #print(len(X))#8820
 
 
@@ -199,11 +245,16 @@ while CONTINUE:
       if pre >= (NFRAME / RATE):
         play = False
         #CONTINUE = False
-
-      output_block = input_block
+      output_block = [0] * BLOCKLEN*CHANNELS
+      for i in range(0, len(input_block)):
+        output_block[i] =int( input_block[i] * (gain.get() /10))
 
       binary_data = struct.pack('hh' * BLOCKLEN, *output_block)
-      stream.write(binary_data)
+      if not skip :
+        stream.write(binary_data)
+      else:
+        sv.set(sv.get() + 0.1)
+
   else:
     time.sleep(0.1)
 
